d9a87e80c218149f72fbb71fd4223bdff24893a1
zsjyxyl <zsjyxyl@163.com>
2021-12-15 01:34:12 +0800

    remove redundant shells

--- run-part.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-#sbt "test:runMain multiplier.test"
-folder="/home/jxzhang/projects/multiplier-generator/benchmarks/wt"
-sbt "test:runMain partialprod.test"
--- run-wt.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-#sbt "test:runMain multiplier.test"
-folder="/home/jxzhang/projects/multiplier-generator/benchmarks/wt"
-sbt "test:runMain wallace.test --wallace-file ${folder}/8output0"
